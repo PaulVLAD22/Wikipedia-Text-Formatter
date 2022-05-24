4c19e049dfe69af0d644bc28511758266f884e38
Vlad Munteanu <paulvlad34@gmail.com>
2022-05-24 17:06:34 +0300

    fix: dark mode + started working on regex

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
-import {Button, ChakraProvider, ColorModeProvider, Flex, Text, Textarea, useColorMode } from '@chakra-ui/react'
+import {Button, ChakraProvider, ColorModeProvider, Flex, Text, Textarea, useColorMode, useColorModeValue } from '@chakra-ui/react'
 import {BsFillCloudSunFill} from "react-icons/bs"
 import { useState } from 'react';
+import DarkMode from "./DarkMode"
 
 function App() {
   const [text, setText] = useState("")
-  const { colorMode, toggleColorMode } = useColorMode();
+  const bg = useColorModeValue('white','gray.800')
+  const fontColor = useColorModeValue("black","white")
+
+  const handleInputChange = (e) => {
+    let inputValue = e.target.value
+    setText(inputValue)
+  }
   const formatText = () => {
-  console.log(toggleColorMode)
+    console.log(text.replace("/ \[[0-9]+\]/g",""))
+    setText(text.replace("\[[0-9]+\]",""))
   }
   return (
-    <ChakraProvider>
+    
         <Flex className="App" w="100vw" h="100vh" justifyContent="center" alignItems="center" bg="gray.200">
-          <Flex w="50%" flexDir="column" gap="40px" boxShadow={"dark-lg"} px="20" py="10" borderRadius={"3xl"} bg="white" alignItems={"center"}>
+          <Flex w="50%" flexDir="column" gap="40px" boxShadow={"dark-lg"} px="20" py="10" borderRadius={"3xl"} bg={bg} alignItems={"center"} textColor={fontColor}  >
             <Text fontFamily={""} textAlign={"center"} fontSize="5xl">Wikipedia Text Formatter</Text>
-            <Button onClick={toggleColorMode}>
-              <BsFillCloudSunFill></BsFillCloudSunFill>
-            </Button>
-            <Textarea h="200px" placeholder='Paste your text here...'></Textarea>
+            <DarkMode/>
+            <Textarea h="200px" placeholder='Paste your text here...' value={text}
+        onChange={handleInputChange}></Textarea>
             <Flex justifyContent={"space-around"} w="100%">
               <Button onClick={formatText} colorScheme={"blue"}>Format</Button>
               <Button colorScheme={"green"}>Copy to Clipboard</Button>
             </Flex>
           </Flex>
         </Flex>
-    
-    </ChakraProvider>
   );
 }
 
